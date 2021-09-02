d382773a24598db8b7ea394ccac1699791b4ef18
amabelle <amabelletr@gmail.com>
2021-09-02 14:04:32 +0300

    Favorites context structure without content

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,19 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home, Favorites } from "pages";
 import { ThemeProvider } from "theme";
+import { FavoritesProvider } from "context/favorites";
 import NavBar from "components/NavBar";
 
 const AppRouter = () => {
   return (
-    <ThemeProvider>
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/favorites" component={Favorites} />
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <FavoritesProvider>
+      <ThemeProvider>
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/favorites" component={Favorites} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </FavoritesProvider>
   );
 };
 
---
+++ src/context/favorites/useFavoritesState.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const useFavoritesState = () => {
+  return <div></div>;
+};
+
+export default useFavoritesState;
