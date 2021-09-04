160d84ed752960facba2d3eef2306fbbb8897e38
amabelle <amabelletr@gmail.com>
2021-09-04 15:46:12 +0300

    Redirect

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import { HashRouter as Router, Switch, Route } from "react-router-dom";
 // Changed from HashRouter, both work
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { Home, Favorites } from "pages";
 import { ThemeProvider } from "theme";
 import { FavoritesProvider } from "context/favorites";
@@ -16,6 +16,7 @@ const AppRouter = () => {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/favorites" component={Favorites} />
+            <Redirect to="/" />
           </Switch>
         </Router>
       </ThemeProvider>
